b88c54531ebd48fbe08d58c27fdb5c5b1b3ef648
Andile <andile.goba@younglings.africa>
2022-11-29 13:16:35 +0200

    middleware server configuration

--- server/server.js
+++ server/server.js
@@ -12,3 +12,16 @@ import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/post.js';
 import { users, posts } from './data/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginEmbedderPolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use('/assets', express.static(path.joiin(__dirname, "public/assets")));
