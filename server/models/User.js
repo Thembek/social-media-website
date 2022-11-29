e980b4bca26e011c62cbc0b3865a84397d31cb09
Andile <andile.goba@younglings.africa>
2022-11-29 11:20:54 +0200

    User model update commit

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,45 @@
-import 
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+    {
+        fisrtName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50,
+        },
+        email: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50,
+        },
+        password: {
+            type: String,
+            reqiured: true,
+            min: 5,
+        },
+        picturePath: {
+            type: String,
+            default: "",
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        location: String,
+        occupatoin: String,
+        viewdProfile: Number,
+        impressions: Number,
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
